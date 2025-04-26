4a037e0f76e096fc17a599816137f861bd1b7ce9
affanTarique <reply2affankhan@gmail.com>
2025-04-26 11:36:24 +0530

    Updated Day1 index.html, Day2 JS questions and folder rename

--- Week 02 More about JS/Day2/Q1.js
+++ Week 02 More about JS/Day2/Q1.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //create counter;
 let counter = 0;
 
@@ -7,3 +8,14 @@ const updatecounter = () => {
 };
 
 setInterval(updatecounter, 1000);
+=======
+//create counter;
+let counter = 0;
+
+const updatecounter = () => {
+    counter++;
+    console.log(counter);
+};
+
+setInterval(updatecounter, 1000);
+>>>>>>> c412993 (Updated Day1 index.html and Day2 JS questions)
--- Week 02 More about JS/Day2/Q2.js
+++ Week 02 More about JS/Day2/Q2.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 let counter = 0;
 
 const updateCounter = () => {
@@ -8,3 +9,15 @@ const updateCounter = () => {
 };
 
 updateCounter();
+=======
+let counter = 0;
+
+const updateCounter = () => {
+    counter++;
+    console.log(counter);
+
+    setTimeout(updateCounter, 1000); 
+};
+
+updateCounter();
+>>>>>>> c412993 (Updated Day1 index.html and Day2 JS questions)
