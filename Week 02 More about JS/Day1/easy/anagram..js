53f10e3b781706c28e1602f31d5bae9d6777f94b
affanTarique <reply2affankhan@gmail.com>
2025-07-09 22:51:06 +0530

    js code

--- Week 02 More about JS/Day1/easy/anagram..js
+++ Week 02 More about JS/Day1/easy/anagram..js
@@ -3,5 +3,6 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp. */
 
   function isAnagram(str1, str2) {
-    
+    const sortedstr1 = str1.
+
   }
