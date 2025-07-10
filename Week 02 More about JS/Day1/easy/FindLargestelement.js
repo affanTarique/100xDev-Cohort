8526421e88a56a821a383c82d235f472b135e02f
affanTarique <reply2affankhan@gmail.com>
2025-07-10 20:09:52 +0530

    js

--- Week 02 More about JS/Day1/easy/FindLargestelement.js
+++ Week 02 More about JS/Day1/easy/FindLargestelement.js
@@ -13,4 +13,7 @@ function findLargestElement(arr) {
 
 }
 numbers = [3, 7, 2, 9, 1];
-
+console.log(findLargestElement(numbers)); // Output: 9
+// This function iterates through the array and compares each element with the current largest number.
+// We took a array input and pass it into a function and after that hmne uske pehle element ko largest number assume kr liya phr usko sabhi rest of compare kr rhe hai
+// if woh lasgest number hi last hai toh wahi rhega wrna value swap ho jyega like jo if statement ke andar ho rha !
