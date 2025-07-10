9f2b10c7b5f7467592ae8256e516bd7c478135fd
affanTarique <reply2affankhan@gmail.com>
2025-07-10 20:06:21 +0530

    js

--- Week 02 More about JS/Day1/easy/FindLargestelement.js
+++ Week 02 More about JS/Day1/easy/FindLargestelement.js
@@ -5,7 +5,8 @@ function findLargestElement(arr) {
     let largestNum = numbers[0];
     for (let i = 0; i < numbers.length ; i++) {
         if (numbers [i] > largestNum[i]) {
-            largestNum = 
+            largestNum = numbers[i];
+            
         } 
     }
 
