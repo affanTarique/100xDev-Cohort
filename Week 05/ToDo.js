e738981b9cf9b77903cce57d5f5d608d0ee68bd7
affanTarique <reply2affankhan@gmail.com>
2025-08-06 01:03:49 +0530

    to

--- Week 05/ToDo.js
+++ Week 05/ToDo.js
@@ -1,4 +1,10 @@
 <html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <title>Todo List</title>
+        <link href="style.css" rel="stylesheet" type="text/css" />
+    </head>
     <body>
 
     </body>
