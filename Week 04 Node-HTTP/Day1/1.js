5897b429d7a229155740aa06224e95188d8c01d8
affanTarique <reply2affankhan@gmail.com>
2025-08-16 11:06:14 +0530

    theable

--- Week 04 Node-HTTP/Day1/1.js
+++ Week 04 Node-HTTP/Day1/1.js
@@ -10,6 +10,18 @@ console.log(ans);
 //cmi---command line interference ---- npm and pwd ek cmi hai... ok...
 
 //thenble in js--
+
+//Yeh code ek example hai ki await kisi bhi object ko resolve kar deta hai agar uske andar .then function hai. 1 second wait karega aur phir "hello world" print karega.
 const thenable = {
-    then: function(onFulfilled)
+    then: function(onFulfilled) {
+        setTimeout(() => onFulfilled(42), 1000); 
+    }
+
+};
+
+async function main() {
+    const v = await thenable;
+    console.log ("hello world")
 }
+
+main();
