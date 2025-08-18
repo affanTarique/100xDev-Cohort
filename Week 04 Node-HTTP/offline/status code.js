dffb4795a1560e71d579a75467fa7c83131d559c
affanTarique <reply2affankhan@gmail.com>
2025-08-18 21:47:15 +0530

    http solution

---
+++ Week 04 Node-HTTP/offline/status code.js
@@ -0,0 +1,70 @@
+const express = require("express")
+const app = express();
+
+var users = [{
+    name: 'John',
+    kidneys: [{
+        healthy: false
+    }, {
+        healthy: true
+    }]
+}]
+
+app.get("/", function (req, res) {
+    
+})
+
+app.post("/", function (req, res) {
+    
+})
+
+app.put("/", function (req, res) {
+    
+})
+
+app.delete("/", function (req, res) {
+    
+})
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+//-------------------------------
+const express = require("express")
+const app = express();
+
+const users = [{
+    name: "John",
+    kidneys: [{
+        healthy: false
+    }]
+}];
+
+app.get("/", function (req, res) {
+    const johnKidneys = users[0].kidneys;
+    const numberOfKidneys = johnKidneys.length;
+    let numberOfHealthyKidneys = 0;
+    for (let i = 0; i < johnKidneys.length; i++) {
+        if (johnKidneys[i].healthy) {
+            numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
+        }
+    }
+
+    const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+    res.json({
+        numberOfKidneys,
+        numberOfHealthyKidneys,
+        numberOfUnhealthyKidneys
+    })
+})
+
+app.post("/", function (req, res) {
+
+})
