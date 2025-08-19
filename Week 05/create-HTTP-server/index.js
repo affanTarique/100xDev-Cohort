0a4aa5ea2e9beb7d4b1d02aceef6491f28003d66
affanTarique <reply2affankhan@gmail.com>
2025-08-19 12:48:38 +0530

    boiler http

--- Week 05/create-HTTP-server/index.js
+++ Week 05/create-HTTP-server/index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+
+const app = express();
+
+app.get("/sum", function(req , res) {
+
+} );
+
+app.get("/sum", function(req , res) {
+
+} );
+
+app.get("/sum", function(req , res) {
+
+} );
+
+app.get("/sum", function(req , res) {
+
+} );
