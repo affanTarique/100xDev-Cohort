e067e11f5edc75be0f9c641e3a780ad15be93a61
affanTarique <reply2affankhan@gmail.com>
2025-08-19 12:32:28 +0530

    middleware

---
+++ Week 05/Middlewere/1.js
@@ -0,0 +1,5 @@
+//middlewere
+
+//an express app is essentially a series of  middleware calls
+
+//https://github.com/100xdevs-cohort-2/assignments/blob/master/week-3/01-middlewares/01-requestcount.js
